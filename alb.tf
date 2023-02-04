8e81cf82ae90cc810efc13dab41af2c1f1a2824b
Johnpaul <seanlite94@gmail.com>
2023-02-04 02:02:55 +0100

    create the ELB

--- alb.tf
+++ alb.tf
@@ -1,3 +1,12 @@
+# Create Application Load balancer
+resource "aws_lb" "lb" {
+  name               = "ALB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.igw_sg.id, ]
+  subnets            = aws_subnet.public_subnets.*.id
+}
+
 # Create target group
 resource "aws_lb_target_group" "tg" {
   name        = "TargetGroup"
@@ -7,13 +16,10 @@ resource "aws_lb_target_group" "tg" {
   vpc_id      = aws_vpc.ugwulo_vpc.id
 
   health_check {
-    enabled             = true
-    interval            = var.health_check["interval"]
-    path                = var.health_check["path"]
-    timeout             = var.health_check["timeout"]
-    matcher             = var.health_check["matcher"]
-    healthy_threshold   = var.health_check["healthy_threshold"]
-    unhealthy_threshold = var.health_check["unhealthy_threshold"]
+    port     = var.health_check.port
+    matcher  = var.health_check.matcher
+    path     = var.health_check.path
+    protocol = var.health_check.protocol
   }
 }
 
@@ -26,14 +32,7 @@ resource "aws_alb_target_group_attachment" "tgattachment" {
 }
 
 
-# Create Application Load balancer
-resource "aws_lb" "lb" {
-  name               = "ALB"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.igw_sg.id, ]
-  subnets            = aws_subnet.public_subnets.*.id
-}
+
 
 # Create a listener
 resource "aws_lb_listener" "web_server" {
@@ -42,31 +41,7 @@ resource "aws_lb_listener" "web_server" {
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-
-# Listener Rule
-resource "aws_lb_listener_rule" "static" {
-  listener_arn = aws_lb_listener.web_server.arn
-  priority     = 100
-
-  action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
-
-  }
-
-  condition {
-    path_pattern {
-      values = ["/var/www/html/index.html"]
-    }
   }
 }
