e77c9b1750774afef5f812d8ac5fd8264d68ddd8
Johnpaul <seanlite94@gmail.com>
2023-02-04 02:16:12 +0100

    create route53 domain

--- route53.tf
+++ route53.tf
@@ -1,17 +1,17 @@
-#create a route53 hosted zone
-resource "aws_route53_zone" "hosted_zone" {
+# Create route53
+resource "aws_route53_zone" "ugwulo" {
   name = var.domain_name
 }
 
-#create a record set inside the hosted zone created earlier
-resource "aws_route53_record" "record" {
-  zone_id = aws_route53_zone.hosted_zone.zone_id
+# Create an A record sub-domain
+resource "aws_route53_record" "a_record" {
+  zone_id = aws_route53_zone.ugwulo.zone_id
   name    = var.sub_domain_name
   type    = "A"
 
   alias {
     name                   = aws_lb.lb.dns_name
-    zone_id                = aws_route53_zone.hosted_zone.zone_id
+    zone_id                = aws_lb.lb.zone_id
     evaluate_target_health = true
   }
 }
